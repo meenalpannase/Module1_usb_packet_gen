767dbbaa943a82cc4063950c0957252605d90628
Meenal <114145036+meenalpannase@users.noreply.github.com>
2022-10-11 13:01:53 +0530

    First commit

--- package_dpram.sv
+++ package_dpram.sv
@@ -1,10 +1,4 @@
 package package_dpram;
-	`include "transaction.sv"
-	`include "packet_generator.sv"
-	`include "driver.sv"
-	`include "monitor.sv"
-	`include "scoreboard.sv"
-	`include "environment.sv"
-	`include "test.sv"
+	
 endpackage
 
